acf95cc681959075ea97eb1326f2db7d9e84683f
EC2 Default User <ec2-user@ip-172-31-33-230.us-east-2.compute.internal>
2019-05-09 19:42:26 +0000

    fix merge conflict

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -16,25 +16,11 @@ class PurchasesController < ApplicationController
 
     def set_payment
         create_payment_info
-<<<<<<< HEAD
-        # redirect_to shipping_methods_path
-=======
->>>>>>> shipping_combine
         create_shipping_method
         redirect_to review_purchase_path
     end
 
-    # def shipping_methods
-    # end
 
-    # def set_shipping_method
-<<<<<<< HEAD
-    #     # create_shipping_method
-    #     # redirect_to review_purchase_path
-=======
-    #     create_shipping_method
-    #     redirect_to review_purchase_path
->>>>>>> shipping_combine
     # end
 
     def review_purchase
@@ -60,7 +46,7 @@ class PurchasesController < ApplicationController
     end
 
     private
-<<<<<<< HEAD
+
     def check_stock
         @card_quantity_hash = @customer.shopping_cart.uniq_cards_quantity_hash
         @card_quantity_hash.each do |card, quantity_in_cart|
@@ -73,7 +59,7 @@ class PurchasesController < ApplicationController
             end
         end
     end 
-=======
+
     
     def full_info
         session[:first_name] = params[:first_name]
@@ -94,7 +80,7 @@ class PurchasesController < ApplicationController
     end
     
     
->>>>>>> shipping_combine
+
     def create_address
         session[:first_name] = params[:first_name]
         session[:last_name] = params[:last_name]
